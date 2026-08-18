d4a6e5d7b583c83716d9d2e67b10944a39bb0759
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-08-18 19:05:30 +0200

    Modify post-create.sh to avoid zsh setup
    Updated the post-create script to skip zsh installation.

--- src/js-base/post-create.sh
+++ src/js-base/post-create.sh
@@ -4,8 +4,8 @@ set -e
 # Fix ownership of the shared volume mounts (these only exist at runtime).
 sudo chown node /.pnpm-store /commandhistory
 
-# Run base OS setup.
-curl -o- https://raw.githubusercontent.com/s-h-a-d-o-w/my-os-basics/refs/heads/main/init.sh | bash -s -- no-nvm no-user-host-in-prompt
+# Run base OS setup. The MS JS base image comes with nvm and zsh.
+curl -o- https://raw.githubusercontent.com/s-h-a-d-o-w/my-os-basics/refs/heads/main/init.sh | bash -s -- no-nvm no-zsh no-user-host-in-prompt
 
 # zsh history settings.
 cat >> ~/.zshrc <<'EOF'
